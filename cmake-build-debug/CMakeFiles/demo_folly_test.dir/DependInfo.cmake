14768cf9a5156eba42060dee085981e41efbe537
yu <386647608@qq.com>
2021-12-19 16:59:02 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/demo_folly_test.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yuhaibo/CLionProjects/demo_folly_test/main.cpp" "/Users/yuhaibo/CLionProjects/demo_folly_test/cmake-build-debug/CMakeFiles/demo_folly_test.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/wangle/2021.12.13.00/include"
+  "/usr/local/Cellar/folly/2021.12.13.00/include"
+  "/usr/local/Cellar/gflags/2.2.2/include"
+  "/usr/local/Cellar/openssl@1.1/1.1.1m/include"
+  "/usr/local/Cellar/boost/1.76.0/include"
+  "/usr/local/Cellar/glog/0.5.0/include"
+  "/usr/local/Cellar/double-conversion/3.1.6/include"
+  "/usr/local/Cellar/libevent/2.1.12/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/demo_folly_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_folly_test.dir/main.cpp.o"
+  "demo_folly_test"
+  "demo_folly_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo_folly_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
